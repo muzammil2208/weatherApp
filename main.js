ca81679925e87068884286c64d1c8167433048e6
muzammil2208 <muzammilsk90@gmail.com>
2023-09-19 12:32:57 +0530

    completed basic functionality of multiple day forecast

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ async function getCityName() {
 async function getWeatherData(cityName)
 {
     try{
-        let response=await fetch("http://api.weatherapi.com/v1/current.json?key=84781dcc9be14da3ab555707231409&q="+cityName+"&aqi=no");
+        let response=await fetch("http://api.weatherapi.com/v1/forecast.json?key=84781dcc9be14da3ab555707231409&q="+cityName+"&days=5&aqi=no&alerts=no");
         let data= await response.json();
         return data;
     }
@@ -36,13 +36,22 @@ async function getWeatherData(cityName)
 
 async function updateAlldata(data,cityName)
 {
+    
+    let day1=data.forecast.forecastday[1];
+    let day2=data.forecast.forecastday[2];
+    let day3=data.forecast.forecastday[3];
+    let day4=data.forecast.forecastday[4];
     const searchInput=document.getElementsByClassName("search")[0];
     const weatherStatus=document.getElementsByClassName("weather_status")[0];
     const raindetails=document.getElementsByClassName("rainChance")[0];
     const winddetails=document.getElementsByClassName("windSpeed")[0];
     const perdetails=document.getElementsByClassName("percipitation")[0];
     const main_temp=document.getElementsByTagName("p")[0];
-
+    const main_image=document.getElementsByClassName("main_image")[0];
+    const day1Field=document.getElementById("day1");
+    const day2Field=document.getElementById("day2");
+    const day3Field=document.getElementById("day3");
+    const day4Field=document.getElementById("day4");
     //updating all information
     searchInput.value=cityName;
     increasewidth();
@@ -51,7 +60,13 @@ async function updateAlldata(data,cityName)
     winddetails.innerHTML=data.current.wind_mph+ "kph";
     perdetails.innerHTML=data.current.humidity+"%";
     main_temp.innerHTML=data.current.temp_c;
+    main_image.src=data.current.condition.icon;
 
+    //updating day information
+    day1Field.innerHTML=day1.day.maxtemp_c+" / "+day1.day.mintemp_c;
+    day2Field.innerHTML=day2.day.maxtemp_c+" / "+day2.day.mintemp_c;
+    day3Field.innerHTML=day3.day.maxtemp_c+" / "+day3.day.mintemp_c;
+    day4Field.innerHTML=day4.day.maxtemp_c+" / "+day4.day.mintemp_c;
 }
 
 
